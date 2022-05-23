22a5395a4578a49048a4128be3b11275ea393bb5
Kuldeep Rana <kuldeep.rana@puresoftware.com>
2022-05-23 22:58:58 +0530

    added swagger doc 3

--- src/main/java/com/codersdesks/config/SwaggerConfig.java
+++ src/main/java/com/codersdesks/config/SwaggerConfig.java
@@ -1,20 +1,12 @@
 package com.codersdesks.config;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+
+
 }
